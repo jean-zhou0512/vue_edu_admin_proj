2e5b9bf33ecc4f2b8623e35bfb64c1f1cc4f34ba
zjm <2434153436@qq.com>
2021-02-03 20:10:17 +0800

    图片上传组件

--- src/views/edu/teacher/save.vue
+++ src/views/edu/teacher/save.vue
@@ -26,14 +26,15 @@
           >更换头像</el-button>
 
           <!--
-          v-show:是否显示上传组件
+          value:是否显示上传组件
           :key:类似于id，如果一个页面多个图片上传控件，可以做区分
           :url: 后台上传的url地址
           @close:关闭上传组件
           @crop-upload-success:上传成后的回调
           -->
           <ImageCropper
-          v-show="false"></ImageCropper>
+          :value="isUoloadImg"
+          url=""></ImageCropper>
         </el-form-item>
 
         <el-form-item>
@@ -61,6 +62,7 @@
             level:'',
             avatar: '',
           },
+          isUoloadImg:false,
           saveBtnDisabled: false,// 保存按钮是否禁用,
           teacherTitleList:[],
           rules:{
