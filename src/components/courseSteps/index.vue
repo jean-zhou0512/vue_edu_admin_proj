d231f72ac2ec7ca47f3238fef1869af7bb858fa8
jean-zhou0512 <2434153436@qq.com>
2021-02-08 20:07:45 +0800

    课程管理添加

--- src/components/courseSteps/index.vue
+++ src/components/courseSteps/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <h2 style="text-align: center;">发布新课程</h2>
-    <el-steps :active="active" process-status="wait" finish-status="success" style="margin-bottom: 40px;">
-      <el-step title="填写课程基本信息"/>
-      <el-step title="创建课程大纲"/>
-      <el-step title="发布课程"/>
-    </el-steps>
-  </div>
+    <div>
+      <h2 style="text-align: center;">发布新课程</h2>
+      <el-steps :active="active" finish-status="success" process-status="wait" align-center style="margin-bottom: 40px;">
+        <el-step title="填写课程基本信息"/>
+        <el-step title="创建课程大纲"/>
+        <el-step title="发布课程"/>
+      </el-steps>
+    </div>
 </template>
 
 <script>
