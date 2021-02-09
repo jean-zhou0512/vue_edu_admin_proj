06c3c3cd3405c9c7d6243dff74bfaecc4760e846
zjm <2434153436@qq.com>
2021-02-09 16:20:04 +0800

    课程管理添加

--- src/components/courseSteps/index.vue
+++ src/components/courseSteps/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <h2 style="text-align: center;">发布新课程</h2>
-      <el-steps :active="active" finish-status="success" process-status="wait" align-center style="margin-bottom: 40px;">
+      <el-steps :active="active"  finish-status="success" process-status="wait" align-center style="margin-bottom: 40px;">
         <el-step title="填写课程基本信息"/>
         <el-step title="创建课程大纲"/>
         <el-step title="发布课程"/>
